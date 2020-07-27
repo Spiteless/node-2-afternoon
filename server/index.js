07dc1d060afe6d6b0785ff6ea0ea190ea4bb9804
Trillium <spiteless@gmail.com>
2020-07-27 14:47:34 -0700

    Completed step 1, 2, 3

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const port = 4001;
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`Server listening on ${port}`)
+})
